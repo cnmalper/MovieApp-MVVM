000348c76d436be9aaaa7ffb878d3e4973392d74
Alper Canimoglu <teknoaydin24@gmail.com>
2024-02-03 21:06:53 +0300

    Initial Commit

---
+++ MovieApp-MVVM/ViewModel/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  MovieApp-MVVM
+//
+//  Created by Alper Canımoğlu on 3.02.2024.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+}
